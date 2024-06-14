f5ded05c283fecadf2dc658081c1d90207d4dc32
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2024-06-14 12:06:09 +0000

    Prune

--- src/trans/gpu/algor/module/IPC_Alltoall.cu
+++ src/trans/gpu/algor/module/IPC_Alltoall.cu
@@ -2,4 +2,5 @@ extern "C" void initIPC(double* output_d,int* roff, int mtol_or_ltom){
 }
 extern "C" int Alltoallv_CUDAIPC(double* input, int* len, int* soff, 
 				  double* output, int* roff,int mtol_or_ltom){
+  return 0;
 }
